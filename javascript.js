7cf6eceb92b7ecf4e30d33fcdf7cd2a815a3fb4c
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2026-03-10 11:43:32 -0700

    Add function to display books in library

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const library = [
-    
+    {author: "Harper Lee", title: "To Kill a Mockingbird", pages: "281", status: "Not Read Yet"},
+    {author: "F. Scott Fitzgerald", title: "The Great Gatsby", pages: "180", status: "Read"},
 ];
 
 function Book(author, title, pages, status) {
@@ -15,3 +16,16 @@ function addBookToLibrary(author, title, pages, status) {
     const newBook = new Book(author, title, pages, status);
     library.push(newBook);
 }
+
+const container = document.querySelector(".container");
+
+function displayBook(){
+    library.forEach((book) => {
+        let card = document.createElement("div");
+        container.appendChild(card);
+        card.textContent = `Title: ${book.title} Author: ${book.author} Pages: ${book.pages} Status: ${book.status}`;
+    })
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "Not Read Yet");
+displayBook();
